798d5187169b54c3b04260792043ab818291ce09
Tung Nguyen <tungnvan.mar12th@gmail.com>
2020-12-08 16:40:10 +0700

    Drew circle tree, radial tree

--- static/pages/hierachical.js
+++ static/pages/hierachical.js
@@ -11,7 +11,7 @@ function drawCirclePack(root) {
   const circleRoot = d3.pack()
     .size([700, 700])
     .padding(5)(root);
-  console.log(circleRoot);
+  // console.log(circleRoot);
 
   const allPackG = svg.selectAll('g.pack').data(circleRoot.descendants());
   
@@ -39,6 +39,77 @@ function drawCirclePack(root) {
   allPackG.exit().remove();
 }
 
+function radialProject(x, y, cx, cy) {
+  if (x === cx && y === cy) {
+    return [x, y];
+  }
+  const tanA = (y - cy) / (x - cx);
+  const a = Math.atan(tanA);
+  const r = Math.sqrt(Math.pow(x - cx, 2) + Math.pow(y - cy, 2));
+  return [r * Math.cos(2 * a) + cx, r * Math.sin(2 * a) + cy];
+}
+
+function drawTree(root) {
+  const canvasPaddingTop = 10;
+  const canvasPaddingLeft = 10;
+  const canvasWidth = 700;
+  const canvasHeight = 700;
+
+  const svg = d3.select('svg#tree');
+
+  const nodeFill = d3.scaleOrdinal()
+    .domain([0, 1, 3, 2])
+    .range(['#364f6b', '#3fc1c9', '#f5f5f5', '#fc5185']);
+
+  const treeRoot = d3.tree()
+    .size([canvasWidth / 2.5, canvasHeight / 2.5])(root);
+  console.log(treeRoot);
+
+  const canvas = svg.select('g#treecanvas')
+    .attr('transform', `translate(${
+      canvasPaddingLeft + canvasWidth / 2 - treeRoot.x
+    }, ${
+      canvasPaddingTop + canvasHeight / 2
+    })`);
+
+  const allLink = canvas.selectAll('path.link').data(treeRoot.descendants());
+  const allNode = canvas.selectAll('g.node').data(treeRoot.descendants());
+
+  allLink.enter()
+    .append('path')
+    .attr('class', 'link')
+    .attr('stroke', (d) => nodeFill(d.depth))
+    .attr('fill', 'none')
+    .attr('d', (d) => {
+      if (d.parent) {
+        const rotatedChild = radialProject(d.x, d.y, treeRoot.x, treeRoot.y);
+        const rotatedParent = radialProject(d.parent.x, d.parent.y, treeRoot.x, treeRoot.y);
+        return d3.linkVertical()({
+          source: [rotatedChild[0], rotatedChild[1]], 
+          target: [rotatedParent[0], rotatedParent[1]],
+        });
+      }
+      return '';
+    }); 
+
+  allNode.enter()
+    .append('g')
+    .attr('class', 'node')
+    .attr('transform', (d) => {
+      const rotated = radialProject(d.x, d.y, treeRoot.x, treeRoot.y);
+      return `translate(${rotated[0]} ${rotated[1]})`;
+    })
+    .each(function(d, i) {
+      const circle = d3.select(this).append('circle');
+      circle
+        .attr('r', 10)
+        .attr('fill', nodeFill(d.depth));
+    });
+
+  allLink.exit().remove();
+  allNode.exit().remove();
+}
+
 window.addEventListener('load', function() {
   d3.json('/static/data/tweets.json').then((data) => {
     const tweets = data.tweets;
@@ -47,6 +118,7 @@ window.addEventListener('load', function() {
     console.log(root);
 
     drawCirclePack(root);
+    drawTree(root);
   }, (err) => {
     console.error(err);
   });
