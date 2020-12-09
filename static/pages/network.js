3b15e0ef7874929ad099d55a528373c3dba24ba7
Tung Nguyen <tungnvan.mar12th@gmail.com>
2020-12-10 01:08:35 +0700

    Drew network with markers

--- static/pages/network.js
+++ static/pages/network.js
@@ -6,11 +6,11 @@ function randomizeDs() {
 }
 
 function drawBubbles(dataset) {
-  const nodes = dataset.map((v) => ({value: v}));
   const canvasPaddingLeft = 10;
   const canvasPaddingTop = 10;
   const canvasWidth = 700;
   const canvasHeight = 700;
+  const nodes = dataset.map((v) => ({value: v}));
 
   const svg = d3.select('svg#bubbles');
 
@@ -44,7 +44,86 @@ function drawBubbles(dataset) {
   }
 }
 
+function drawNetwork(dataset) {
+  const canvasPaddingLeft = 10;
+  const canvasPaddingTop = 10;
+  const canvasWidth = 700;
+  const canvasHeight = 700;
+  const nodeRadius = 15;
+
+  const {nodes, edges} = dataset;
+
+  const nodeRef = {};
+  for (let i = 0; i < nodes.length; i += 1) {
+    nodeRef[nodes[i].name] = nodes[i];
+  }
+  const links = edges.map((ed) => ({...ed, source: nodeRef[ed.from], target: nodeRef[ed.to]}));
+
+  const svg = d3.select('svg#network');
+
+  const manyBodyForce = d3.forceManyBody().strength(-50);
+  const centerForce = d3.forceCenter()
+    .x(canvasPaddingLeft + canvasWidth / 2)
+    .y(canvasPaddingTop + canvasHeight / 2);
+  const collideForce = d3.forceCollide().radius(nodeRadius);
+  const linkForce = d3.forceLink().links(links).distance(75);
+
+  const force = d3.forceSimulation()
+    .nodes(nodes)
+    .force('center', centerForce)
+    .force('charge', manyBodyForce)
+    .force('collision', collideForce)
+    .force('link', linkForce);
+
+  force.on('tick', update);
+
+  svg.selectAll('line.link').data(links, (d) => `${d.from}-${d.to}`).enter()
+    .append('line')
+    .attr('class', 'link')
+    .attr('x1', (d) => d.source.x)
+    .attr('y1', (d) => d.source.y)
+    .attr('x2', (d) => d.target.x)
+    .attr('y2', (d) => d.target.y)
+    .attr('stroke-width', (d) => d.weight)
+    .attr('stroke', '#bbe1fa')
+    .attr('marker-end', 'url(#arrow)');
+
+  svg.selectAll('g.node').data(nodes, (d) => d.name).enter()
+    .append('g')
+    .attr('class', 'node')
+    .attr('transform', (d) => `translate(${d.x} ${d.y})`)
+    .each(function(d, i) {
+      d3.select(this)
+        .append('circle')
+        .attr('r', nodeRadius)
+        .attr('fill', '#0f4c75');
+      d3.select(this)
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('dy', 5)
+        .attr('font-size', '13px')
+        .attr('fill', '#fff')
+        .text(d.name);
+    });
+
+  function update() {
+    svg.selectAll('line.link')
+      .attr('x1', (d) => d.source.x)
+      .attr('y1', (d) => d.source.y)
+      .attr('x2', (d) => d.target.x)
+      .attr('y2', (d) => d.target.y)
+      .attr('marker-end', 'url(#arrow)');
+    svg.selectAll('g.node')
+      .attr('transform', (d) => `translate(${d.x} ${d.y})`);
+  }
+}
+
 window.addEventListener('load', function() {
-  const sampleData = randomizeDs();
-  drawBubbles(sampleData);
+  // const sampleData = randomizeDs();
+  // drawBubbles(sampleData);
+  d3.json('/static/data/network.json').then((data) => {
+    drawNetwork(data);
+  }, (err) => {
+    console.log(err);
+  });
 });
